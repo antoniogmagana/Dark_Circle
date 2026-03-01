554c4a90e39695fa560b2a2ed20f415b95f49225
Brandon Taylor <btaylor9273@gmail.com>
2026-03-01 01:54:05 +0000

    update ingestor main

---
+++ inference-engine/src/ingestor/main.py
@@ -0,0 +1,12 @@
+import rclpy
+from rclpy.node import Node
+import nats
+import asyncio
+import threading
+
+# locat packages
+from buffer import SensorBuffer
+
+# TODO: Import ROS2 message definition
+
+
